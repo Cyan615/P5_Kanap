645f0376ef315deda7b85424a8dc53f006a363ca
N.Maurice <cirilaii.nm@gmail.com>
2022-06-30 19:37:52 +0200

    colors choice

--- front/js/product.js
+++ front/js/product.js
@@ -5,20 +5,20 @@ const id = urlSearchParams.get("id");
 
 
 // affichage du canapé séléctionné par son id
-const fetchProduit = async () => {
-    await fetch(`http://localhost:3000/api/products/${id}`)
-        .then((res) => res.json())
-        .then((promise) => choiceData(promise));
-};
-fetchProduit()
+fetch(`http://localhost:3000/api/products/${id}`)
+    .then((res) => res.json())
+    .then((promise) => choiceData(promise));
+
 
 // création des éléments de la carte produit sélectionné en page d'acceuil
 
 function choiceData(sofa) {
-    const {imageUrl, altTxt, description, color, price, _id, name} = sofa
+    const {imageUrl, altTxt, description, colors, price, name} = sofa
     makeImage(imageUrl, altTxt)
     makeName(name)
     makePrice(price)
+    makeDescription(description)
+    colorsChoice(colors)
 }
 // création de l'élement image
 function makeImage(imageUrl, altTxt){
@@ -33,8 +33,25 @@ function makeName(name) {
     const title = document.querySelector("#title")
     if (title != null) title.textContent = name    
 }
-// ajout du prix dans la carte canapé
+// ajout du prix dans la carte produit
 function makePrice(price) {
     const prix = document.querySelector("#price")
-    prix.textContent = price
+    if (prix != null) prix.textContent = price
+}
+// ajout de la description du canapé dans la carte produit
+function makeDescription(description) {
+    const sofaDescription = document.querySelector("#description")
+    if (sofaDescription != null) sofaDescription.textContent = description
+}
+// ajouter les parametres de couleur disponible pour le produit séléctionné
+function colorsChoice(colors) {
+    const selectColor = document.querySelector("#colors")
+    if (selectColor != null) {
+        colors.forEach( (color) => {
+            const option = document.createElement('option')
+            option.value = color
+            option.textContent = color
+            selectColor.appendChild(option)
+        })
+    }
 }
